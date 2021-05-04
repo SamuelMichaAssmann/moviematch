892c8ca05b6f78762f09de41662d31352521945f
SamuelMichaAssmann <52752595+assmannsamuel@users.noreply.github.com>
2021-05-04 15:43:51 +0200

    save

--- backend/main.py
+++ backend/main.py
@@ -1,57 +1,14 @@
 import flask
+import time
 
 app = flask.Flask("__main__")
 
-@app.route("/")
-def my_index():
-    return flask.render_template("index.html", token="MovieMatch")
+@app.route("/api")
+def version():
+    return "Api v0.1.0"
 
-app.run(debug=True)
+@app.route('/api/time')
+def get_current_time():
+    return {'time': time.time()}
 
-#!/usr/bin/env python3
-from flask import Flask, request, jsonify, Response
-import uuid
-
-app = Flask(__name__)
-autos = []
-
-@app.route('/autos')
-def get_autos():
-  return jsonify({'gebrauchtwagen': autos})
-
-def checkPostObjekt(post_objekt):
-  regeln = [len(post_objekt) == 3,
-          'modell' in post_objekt,
-          'preis' in post_objekt,
-          'baujahr' in post_objekt]
-  if all(regeln):
-    return True
-  else:
-    return False
-
-@app.route('/autos', methods=['POST'])
-def add_auto():
-  post_content = request.get_json()
-  if checkPostObjekt(post_content):
-    post_content['id'] = uuid.uuid4().hex
-    autos.append(post_content)
-    success = Response('', status=201)
-    success.headers['Location'] = '/autos/' + post_content['id']
-    return success
-  else:
-    failure = Response('', status=400)
-    return failure
-
-@app.route('/autos/')
-def auto_by_id(id):
-  return_object = {}
-  for auto in autos:
-    if auto['id'] == id:
-      return_object = auto
-  if not return_object:
-    failure = Response('', status=400)
-    return failure
-  else:
-    return jsonify(return_object)
-
-app.run(port=5000)
+app.run(port=5000, debug=True)
