3279917ecbfc9400f9b09aa04ba20ebf20a21d2d
MoritzMueller2508 <moritzmueller2508@gmail.com>
2021-05-12 13:13:24 +0200

    re fb

--- backend/api.py
+++ backend/api.py
@@ -4,25 +4,25 @@ import time
 import firebase_admin  # import firebase_dependencies
 import pyrebase
 import json
-from firebase_admin import credentials, auth
+#from firebase_admin import credentials, auth   - fbAdmin not working -> delete for now
 from flask import Flask, request
-import src.firebase
+from src.firebase import *
 from src.algo import matchfilm
 
 
 # App configuration
 app = flask.Flask("__main__")
 
-
 # Connect to firebase
-cred = credentials.Certificate('fbAdminConfig.json')
-firebase = firebase_admin.initialize_app(cred)
-pb = pyrebase.initialize_app(json.load(open('fbconfig.json')))
+#cred = credentials.Certificate('fbAdminConfig.json') - fbAdmin not working -> delete for now
+#firebase = firebase_admin.initialize_app(cred)       - fbAdmin not working -> delete for now
+pb = pyrebase.initialize_app(json.load(open('F:\\Development\\WebII\\moviematch\\backend\\fbconfig.json')))
+auth = pb.auth
 
 # Data source - unecessary tho
 users = [{'uid': 1, 'name': 'Noah Schrainer'}]
 
-
+'''
 def check_token(f):  # middleware - check for valid token before performing fb_user action
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -35,28 +35,41 @@ def check_token(f):  # middleware - check for valid token before performing fb_u
             return {'message': 'Invalid token provided'}, 400
         return f(*args, **kwargs)
     return wrap
+    ''' # - fbAdmin not working -> delete for now
 
 
 @app.route("/api/signUp")
-# signup
+def signup():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if (email is None or password is None):
+        return {'message': 'Error missing email or password'}, 400
+    try:
+        user = auth.create_user_with_email_and_password(
+            email=email,
+            password=password
+        )
+        return {'message': f'Successfully created user {user.uid}'}, 201
+    except:
+        return {'message': 'Error creating user'}, 400
 
 
 # Api route to get a new token for a valud user
-@app.route("api/token")
+@app.route("/api/token")
 def token():
     email = request.form.get('email')
     password = request.form.get('password')
 
     try:
-        user = pb.auth().sign_in_with_email_and_password(
+        user = auth.sign_in_with_email_and_password(
             email,
             password
         )
         jwt = user['idToken']
         return {'token': jwt}, 200
     except:
-        return {'message': 'There was an error logging in'}, 400
-
+        return {'message': 'There was an error logging in'}, 400   
 
 @app.route("/api")
 def version():
@@ -71,6 +84,7 @@ def get_current_time():
 @app.route('/api/match')
 def getMovieData():
     return matchfilm()
+    #matchfilm()
 
 
 if __name__ == "__main__":
