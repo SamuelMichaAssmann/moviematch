02524f068d7063101ece4d629d13610a27300695
Samuel <samuel@wasge.de>
2021-05-14 13:50:08 +0200

    firebase

--- backend/src/datamanager/watchlist.py
+++ backend/src/datamanager/watchlist.py
@@ -1,61 +1,78 @@
 import json
 PATH = '../data/firebase_data.json'
 
-def getData():
+
+def getFirebase():
     try:
         with open(PATH) as file:
             return json.load(file)
     except (FileNotFoundError):
         return set()
 
-def getUsers():
-    data = getData()
+
+# kind -> {user, group}
+def getData(id, kind):
+    data = getFirebase()
     try:
-        return data["users"]
+        data = data[kind]
+        return data[id]
     except (KeyError, TypeError):
         return set()
 
 
-def getGroup():
-    data = getData()
+# data -> {getData()}, kind -> {'watchlist', 'antiwatch'}{'watchlist', 'antiwatch', 'matched', 'members'}
+def getList(data, kind):
     try:
-        return data["groups"]
-    except (KeyError, TypeError):
-        return set()
-
-
-def getUserdata(user):
-    data = getUsers()
-    try:
-        return data[user]
-    except (KeyError, TypeError):
-        return set()
-
-
-def getWatchlist(userdata):
-    try:
-        return userdata['watchlist']
+        return data[kind]
     except (KeyError, TypeError):
         return []
 
 
-def getAntiwatch(userdata):
+# kind -> {'user', 'groupe'}, list -> {'name', 'email', 'groups', 'watchlist', 'antiwatch', 'age'}{'name', 'watchlist', 'antiwatch', 'matched', 'members'}
+def setList(kind, id,  list, input):
+    data = getFirebase()
     try:
-        return userdata['antiwatch']
-    except (KeyError, TypeError):
-        return []
-
-
-def setData(user, userdata):
-    data = getData()
-    try:  
-        data[user] = userdata
+        data[kind][id][list] = input
         with open(PATH, 'w') as file:
             json.dump(data, file)
     except (FileNotFoundError, KeyError, TypeError):
         pass
 
+# complete user
+def setUser(id, name, mail, groups, watchlist, antiwatch, age):
+    data = getFirebase()
+    try:
+        data["users"][id] = {"name": name, 
+                             "mail": mail,
+                             "groups": groups,
+                             "watchlist": watchlist,
+                             "antiwatch": antiwatch,
+                             "age": age
+                             }
+        with open(PATH, 'w') as file:
+            json.dump(data, file)
+    except (FileNotFoundError, KeyError, TypeError):
+        return None
 
-print(getData())
-print(getUserdata('12345'))
-print(getWatchlist(getUserdata('12345')))
+
+# complete user
+def setGroupe(id, name, watchlist, antiwatch, matched, members):
+    data = getFirebase()
+    try:
+        if id in data["groups"]:
+            return None
+        data["groups"][id] = {"name": name, 
+                             "watchlist": watchlist,
+                             "antiwatch": antiwatch,
+                             "matched": matched,
+                             "members": members
+                             }
+        with open(PATH, 'w') as file:
+            json.dump(data, file)
+    except (FileNotFoundError, KeyError, TypeError):
+        return None
+
+
+# print(setList('users', '12345', 'watchlist', [1696,418078,284303]))
+setUser("56789", "Debbie", "", [], [], [], "17")
+setGroupe("56789", "aasdsd", [], [], [], [])
