37e020cec4a923720fe9fcad1af3c05799f39104
MoritzMueller2508 <moritzmueller2508@gmail.com>
2021-06-09 16:10:40 +0200

    me stupid

--- frontend/src/components/assets/Navbar/Navbar.js
+++ frontend/src/components/assets/Navbar/Navbar.js
@@ -10,7 +10,7 @@ import { IconContext } from 'react-icons/lib';
 function Navbar() {
 
   let isLoggedIn = false;
-  if (sessionStorage.getItem("uid") != null) {
+  if (localStorage.getItem("uid") != null) {
     isLoggedIn = true;
   }
 
