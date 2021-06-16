f880be328be233b79f83e1dfa4256c5371f50a5f
Samuel <samuel@wasge.de>
2021-06-16 11:58:49 +0200

    fix loading

--- frontend/src/components/assets/SignIn/Register.js
+++ frontend/src/components/assets/SignIn/Register.js
@@ -17,7 +17,7 @@ export class Register extends React.Component {
       password: '',
       passwordConfirmation: '',
       confirmation: false,
-      loading : false
+      loading: false
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -68,12 +68,12 @@ export class Register extends React.Component {
     console.log(response);
     //may need to have a look - states are updated now matter if success or not
 
-    localStorage.setItem('loginState' , 'true')
-    localStorage.setItem('uid' , response['uid'])
+    localStorage.setItem('loginState', 'true')
+    localStorage.setItem('uid', response['uid'])
     localStorage.setItem('email', response['email'])
-    this.setState({ loading : false });
+    this.setState({ loading: false });
     window.location.href = '/tutorial'; //relink to verification - TODO
-    
+
 
     /*
     const args = {
@@ -84,84 +84,81 @@ export class Register extends React.Component {
     const response = await axios.post('http://localhost:5000/api/signup', args);
     console.log(response);
     */
-  
-/*
-    if (this.state.confirmation == true) {
-      firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
-        .then((userCredential) => {
-          //Signed in
-          var user = userCredential.user;
-          user.sendEmailVerification();
-          //redirect to VerificationSite?
-          alert("Success! You are now a registered member of MovieMatch. Please check your mailbox to verify your adress");
-          this.resetForm();
-          
-          window.location.href = '/sign-up';
-        })
-        .catch((error) => {
-          var errorMessage = error.message;
-          alert(errorMessage + "\nPlease try again.");
-          this.resetForm();
 
-        });
-    }
-    else {
-      alert("Passwords did not match");
-      this.resetForm();
-      console.log("resetRunning");
-
-     }*/
+    /*
+        if (this.state.confirmation == true) {
+          firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then((userCredential) => {
+              //Signed in
+              var user = userCredential.user;
+              user.sendEmailVerification();
+              //redirect to VerificationSite?
+              alert("Success! You are now a registered member of MovieMatch. Please check your mailbox to verify your adress");
+              this.resetForm();
+              
+              window.location.href = '/sign-up';
+            })
+            .catch((error) => {
+              var errorMessage = error.message;
+              alert(errorMessage + "\nPlease try again.");
+              this.resetForm();
+    
+            });
+        }
+        else {
+          alert("Passwords did not match");
+          this.resetForm();
+          console.log("resetRunning");
+    
+         }*/
   }
 
   render() {
     return (
       <div className="base-container" ref={this.props.containerRef}>
-        <div className = "loading">
-        {this.state.loading ? <Loading/> : ""} 
-        </div>
-        
-        <div className="header">Register</div>
-        <div className="content">
+        {this.state.loading ? <Loading /> :
           <div>
-            <div className="form">
-              <div className="form-group">
-                
-                <Textfield
-                  needed="true"
-                  type="email"
-                  label='Enter your email'
-                  name='email'
-                  value={this.state.email}
-                  onChange={this.handleChange} />
-              </div>
-              <div className="form-group">
-                <Textfield
-                  type="password"
-                  label='Enter your password'
-                  name='password'
-                  value={this.state.password}
-                  onChange={this.handleChange} />
-              </div>
-              <div className="form-group">
-                <Textfield
-                  type="password"
-                  label='Confirm your password'
-                  name='passwordConfirmation'
-                  value={this.state.passwordConfirmation}
-                  onChange={this.handleChange} />
-              </div>
-              <div>
-                <Button
-                  buttonSize='btn--wide'
-                  buttonColor='blue'
-                  onClick={this.registerFirebase}
-                >
-                  Login
-                </Button>
+            <div className="header">Register</div>
+            <div className="content">
+              <div className="form">
+                <div className="form-group">
+                  <Textfield
+                    needed="true"
+                    type="email"
+                    label='Enter your email'
+                    name='email'
+                    value={this.state.email}
+                    onChange={this.handleChange} />
+                </div>
+                <div className="form-group">
+                  <Textfield
+                    type="password"
+                    label='Enter your password'
+                    name='password'
+                    value={this.state.password}
+                    onChange={this.handleChange} />
+                </div>
+                <div className="form-group">
+                  <Textfield
+                    type="password"
+                    label='Confirm your password'
+                    name='passwordConfirmation'
+                    value={this.state.passwordConfirmation}
+                    onChange={this.handleChange} />
+                </div>
+                <div>
+                  <Button
+                    buttonSize='btn--wide'
+                    buttonColor='blue'
+                    onClick={this.registerFirebase}
+                  >
+                    Login
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        }
       </div>
     );
   }
