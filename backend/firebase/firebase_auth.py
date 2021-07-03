0807853bc7ea299877cfb287a5522f4ff33f49e3
WreckingPrograms <djemiegroot@hotmail.com>
2021-07-03 22:36:11 +0200

    Frontend implementation for verification checking

--- backend/firebase/firebase_auth.py
+++ backend/firebase/firebase_auth.py
@@ -115,10 +115,15 @@ def resend_verification_email(request, allow_refresh=True, new_token='', new_ref
                 return { 'message': 'Could not send a verification email' }, 400
         return { 'message': 'Could not send a verification email' }, 400
       
-def isUserVerified(request):
+'''
+Check if a user has verified their email.
+:param request: Request object containing refresh_token.
+'''
+def is_user_verified(request):
     request_token = request.json['refresh_token']
     user = auth.refresh(request_token)
     if (auth.get_account_info(user['idToken'])['users'][0]['emailVerified']):
-        return {'message' : "User is verified"},200
-    else: return {'message' : "User is not verified"}, 401
+        return {'message': 'User is verified'}, 200
+    else:
+        return {'message': 'User is not verified'}, 401
 
