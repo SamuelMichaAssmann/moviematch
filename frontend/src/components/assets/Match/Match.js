61b9fb13c45b8ec16bebc1547034ff84f85e4f49
Samuel <samuel@wasge.de>
2021-07-04 15:16:11 +0200

    fix css

--- frontend/src/components/assets/Match/Match.js
+++ frontend/src/components/assets/Match/Match.js
@@ -4,59 +4,45 @@ import confetti from "canvas-confetti";
 import { MovieThumbnail } from '../Image/MovieThumbnail';
 import MovieInfo from '../Matching/MovieInfo/MovieInfo';
 
-function Match({
+export const Match = ({
   title,
-  movie_id,
   desc,
   runtime,
   rating,
   genres,
-  thumbnailSrc
-}, onclick
-) {
+  thumbnailSrc,
+  onclick
+}) => {
   confetti({
     particleCount: 300,
     spread: 360,
     startVelocity: 50,
   });
 
-  //const modal = document.querySelector(`.modal`);
-  // const contentWrapper = modal.querySelector(".content-wrapper");
-  // const close = modal.querySelector(".close");
-
-  // close.addEventListener("click", () => modal.classList.remove("open"));
-  //modal.addEventListener("click", () => modal.classList.remove("open"));
-  // contentWrapper.addEventListener("click", (e) => e.stopPropagation());
-  //modal.classList.toggle("open");
-
   return (
-    <>
-      <div class="modal" >
-        <article class="content-wrapper">
-          <button class="close" onclick={onclick}></button>
-          <div className="scrollview">
-            <h2 className='matchheading'>You got a match!</h2>
-            <div className='movieThumbnailRow movieThumbnailRowGroup'>
-              <MovieThumbnail
-                src={thumbnailSrc}
-                height="400"
-              />
-              <div>
-                <h2 className='lightmovieTitle'>{title}</h2>
-                <p className='home__sek-subtitle lightmovieDescription'>{desc}</p>
-              </div>
+    <div class="modal open" >
+      <article class="content-wrapper">
+        <button class="close" onclick={onclick}></button>
+        <div className="scrollview">
+          <h2 className='matchheading'>You got a match!</h2>
+          <div className='movieThumbnailRow movieThumbnailRowGroup'>
+            <MovieThumbnail
+              src={thumbnailSrc}
+              height="400"
+            />
+            <div>
+              <h2 className='lightmovieTitle'>{title}</h2>
+              <p className='home__sek-subtitle lightmovieDescription'>{desc}</p>
             </div>
-            <MovieInfo {... {
-              runtime: runtime,
-              rating: rating,
-              genres: genres,
-              tableExtraClasses: 'movieTableGroup',
-            }} />
           </div>
-        </article>
-      </div>
-    </>
+          <MovieInfo {... {
+            runtime: runtime,
+            rating: rating,
+            genres: genres,
+            tableExtraClasses: 'movieTableGroup',
+          }} />
+        </div>
+      </article>
+    </div>
   );
-}
-
-export default Match;
+};
